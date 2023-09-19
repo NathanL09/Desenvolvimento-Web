6b86a7f8e07d0878486364c66d80a5b241857fc6
Nathan Lins <nathan.lins098@gmail.com>
2023-09-19 19:16:19 -0300

    add correction in the name

--- JavaScript/Class-01- data types/challenges/challenge-03/2-object-game.js
+++ JavaScript/Class-01- data types/challenges/challenge-03/2-object-game.js
@@ -1,9 +1,9 @@
 // Crie um programa para cadastrar Jogo que irá solicitar ao usuário o nome, o gênero e o ano de lançamento do mesmo. Em seguida, monte um OBJECT contendo essas informações e por meio dele e imprima as características do jogo usando a função log.
 
 
-const gameName = prompt("Digite o nome do jogo:");
-const gameGenre = prompt("Digite o gênero do jogo:");
-const gameReleaseYear = prompt("Digite o ano de lançamento do jogo:");
+const gameName = prompt("Digite o nome do jogo: ");
+const gameGenre = prompt("Digite o gênero do jogo: ");
+const gameReleaseYear = prompt("Digite o ano de lançamento do jogo: ");
 
 const game = {
     name: gameName,
