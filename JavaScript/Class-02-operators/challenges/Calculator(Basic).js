c09ec171b57fd91b456cd971de1c71bf0da4ddb9
Nathan Lins <nathan.lins098@gmail.com>
2023-09-20 19:11:57 -0300

    add tag strong

--- JavaScript/Class-02-operators/challenges/Calculator(Basic).js
+++ JavaScript/Class-02-operators/challenges/Calculator(Basic).js
@@ -3,8 +3,8 @@
 // Crie constantes e atribua as operações de:
 // +, -, *, /, %, ** e raiz para os números solicitados.
 
-const number1 = (prompt("Digite o primeiro número para calcular: "))
-const number2 = (prompt("Agora digite o segundo número: "))
+const number1 = Number(prompt("Digite o primeiro número para calcular: "))
+const number2 = Number(prompt("Agora digite o segundo número: "))
 
 alert(`Os números são ${number1} e ${number2}!`)
 
@@ -14,12 +14,13 @@ const mul = number1 * number2
 const div = number1 / number2
 const rem = number1 % number2
 const exp = number1 ** number2
-const sqr = number1 **  (1 / number2)
+const sqr = number2 **  (1 / number1)
 
-document.write (`A soma de ${number1} e ${number2} é de: ${add} <br>`);
-document.write (`A subtração de ${number1} e ${number2} é de: ${sub} <br>`);
-document.write (`A multiplicação de ${number1} e ${number2} é de: ${mul} <br>`);
-document.write (`A divisão de ${number1} e ${number2} é de: ${div} <br>`);
-document.write (`O resto da divisão de ${number1} e ${number2} é de: ${rem} <br>`);
-document.write (`A potência de ${number1} elevado a ${number2} é de: ${exp} <br>`);
-document.write (`A raiz ${number1} de ${number2} é de: ${sqr} <br>`);
+
+document.write (`<strong>A soma de ${number1} + ${number2} é de: ${add}</strong> <br>`);
+document.write (`<strong>A subtração de ${number1} - ${number2} é de: ${sub}</strong> <br>`);
+document.write (`<strong>A multiplicação de ${number1} * ${number2} é de: ${mul}</strong> <br>`);
+document.write (`<strong>A divisão de ${number1} / ${number2} é de: ${div.toFixed(3)}</strong> <br>`);
+document.write (`<strong>O resto da divisão de ${number1} % ${number2} é de: ${rem}</strong> <br>`);
+document.write (`<strong>A potência de ${number1} ^ ${number2} é de: ${exp}</strong> <br>`);
+document.write (`<strong>A raiz ${number1} de ${number2} é de: ${sqr.toFixed(3)}</strong> <br>`);
