cf9a48a4c6b4f6dfd8a825cc8f104e4ba18c2e1d
Nathan Lins <nathan.lins098@gmail.com>
2023-09-20 20:32:29 -0300

    add challenge

---
+++ JavaScript/Class-02-operators/challenges/evenOdd.js
@@ -0,0 +1,6 @@
+// Construa uma lógica para dizer se um número é par ou é ímpar
+// Receba o número desejado do usuário via promp
+// Escreve na tela o número e se ele é par ou é ímpar
+
+const numUser = prompt("Digite algum número para ver se ele é par ou impar: ")
+const result = numUser 2 == 
