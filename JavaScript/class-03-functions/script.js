6231eef93f9243146f284b56a5f3024749722ba1
Nathan Lins <nathan.lins098@gmail.com>
2023-09-22 20:08:17 -0300

    add more

--- JavaScript/class-03-functions/script.js
+++ JavaScript/class-03-functions/script.js
@@ -1,5 +1,18 @@
-function average () {
-    console.log("test");
+function average (grade1, grade2) {
+    const result = (grade1 + grade2) /2
+    document.write(`${result} <br>`)
 }
 
-average();
+average(4, 7); // Argument
+average(6, 5); // Argument
+average(9, 7); // Argument
+average(7, 6); // Argument
+average(8, 3); // Argument
+average(8, 10); // Argument
+
+// Anonymous
+const add = function (n1, n2){
+    return n1 + n2
+}
+
+console.log(add(2, 3));
