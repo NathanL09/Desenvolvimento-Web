21c5c4c13d5e5deead758f8dfa136601800d14eb
Nathan Lins <nathan.lins098@gmail.com>
2023-09-25 20:39:45 -0300

    add challenge and class 04

--- JavaScript/class-03-functions/challenge/convertMsToKmh.js
+++ JavaScript/class-03-functions/challenge/convertMsToKmh.js
@@ -8,4 +8,4 @@ return (speed * 3.6) + "km/h"
 
 const userSpeed = prompt("digite um número qualquer para saber qual será a velocidade em km/h")
 const convertSpeed = convertMsToKmh(userSpeed)
-document.write(convertMsToKmh(userSpeed) + "km/h")
+document.write(convertMsToKmh(userSpeed) + "")
