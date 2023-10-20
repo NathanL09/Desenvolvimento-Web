850a408d84821123c4c3e32291a8625d5e38a57d
Nathan Lins <nathan.lins098@gmail.com>
2023-10-20 19:44:53 -0300

    update index for each

---
+++ 4-JavaScript/Class-07-array-methods/2-find-filter.js
@@ -0,0 +1,8 @@
+const studants = [
+    { id: 1, name: "João", average: 6, birth: 2005 },
+    { id: 2, name: "Thiago", average: 6, birth: 2002 },
+    { id: 3, name: "Jonas", average: 5, birth: 1998 },
+    { id: 4, name: "Rosa", average: 9, birth: 2004 },
+    { id: 5, name: "Amanda", average: 8, birth: 1992 },
+    { id: 6, name: "Pedro", average: 7, birth: 1998 },
+  ];
