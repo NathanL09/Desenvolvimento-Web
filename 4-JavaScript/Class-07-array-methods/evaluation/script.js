4c0c2eacb324abf0e5f0980838364af73cc2f671
Nathan Lins <nathan.lins098@gmail.com>
2023-10-27 20:33:02 -0300

    add questions

--- 4-JavaScript/Class-07-array-methods/evaluation/script.js
+++ 4-JavaScript/Class-07-array-methods/evaluation/script.js
@@ -44,26 +44,48 @@ console.log(Math.sqrt(sqrt));
 // Questão 3 (Functions):
 // Crie e execute uma função de nome 'test" que retorne "JavaScript" concatenado com um valor passado como argumento e imprima seu retorno.
 
-function average (g1, g2) {
-    return (g1 + g2) / 2
+function test(params) {
+    return "javaScript " + params
 }
 
-const a = average(9, 7)
-console.log(a);
+console.log(test("Nathan"));
 
 
 // Questão 4 (Data Manipulation):
 // Usando os métodos FIFO e LIFO, crie um array com 3 valos e posteriormente insira mais um valor no final e substitua o do início por outro.
 
+const newArray = [0]
 
+newArray.push("nathan")
+newArray.push("lins")
+
+newArray.shift()
+newArray.unshift("lins")
+
+console.log(newArray);
 
 // Questão 5 (for):
 // Liste todos os valores do array criado na questão anterior ultilizando o for.
+
+const newA = [0, 1]
+
+for (let i = 0; i < newA.length; i++) {
+    const element = newA[i];
+    console.log(newA);
+}
+
 
 
 // Questão 6 (while):
 // Escreva os números de 0 a 500 sem os que pertencem à familía do 200 e do 300:
 
+const count = 0;
+while (count <= 500) {
+    if (count >= 200 || count < 400);
+    continue
+}
+console.log(count)
+count++
 
 // Questão 7 (if):
 // Crie uma lógica para verificar se o motorista foi multado
@@ -71,13 +93,22 @@ console.log(a);
 // E por velocidade baixa ( quando estiver inferior à metade da máxima )
 // A velocidade da pista é de 60km/h
 
+function checkfine(velAtual, maximaVel) {
+    if 
+}
+
 
 // Questão 8 (array methods):
 // Uma empresa vai dar 10% de aumento para seus funcionários
 // Abaixo tem a lista com todos os salários atuais de cada um deles
 // Apresente os novos salários e o novo total em folha dessa empresa
-const salary = [2000, 2800, 3000];
+const salaries = [2000, 2800, 3000];
 
-const salaryInrease = 10 / 100
+const increase = 10 / 100
 
-const newSalary = newSalary.map
+const newSalaries = salaries.map(salary => salary * increase);
+
+const sumSalary = sumSalary.reduce((accumulator, value) => accumulator + value, 0)
+
+console.log(newSalaries);
+console.log(sumSalary); 
